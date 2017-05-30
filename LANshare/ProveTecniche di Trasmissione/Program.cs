a97a088261388995ce4a3d18ffd457b782444b05
Convezione <enrico.panetta@gmail.com>
2017-05-30 18:19:18 +0200

    Port as variable

--- LANshare/ProveTecniche di Trasmissione/Program.cs
+++ LANshare/ProveTecniche di Trasmissione/Program.cs
@@ -45,8 +45,10 @@ namespace ProveTecniche_di_Trasmissione
     class infoSendRec
     {
         private readonly User currentUser;
+        private readonly int port;
         public infoSendRec()
         {
+            port = 42666;
             currentUser = new User();
             currentUser.name = "Tonino";
             currentUser.surname = "Accolla";
@@ -55,7 +57,7 @@ namespace ProveTecniche_di_Trasmissione
 
         public async void InfoServer()
         {
-            TcpListener server = new TcpListener(IPAddress.Any, 42666);
+            TcpListener server = new TcpListener(IPAddress.Any, port);
             server.Start();
             var client = server.AcceptTcpClient();
             var str = client.GetStream();
@@ -68,7 +70,7 @@ namespace ProveTecniche_di_Trasmissione
 
         public async void InfoClient()
         {
-            TcpClient client = new TcpClient("127.0.0.1", 42666);
+            TcpClient client = new TcpClient("127.0.0.1", port);
             var str =client.GetStream();
             byte[] data = new byte[client.ReceiveBufferSize];
             str.Read(data, 0, client.ReceiveBufferSize);
@@ -83,9 +85,11 @@ namespace ProveTecniche_di_Trasmissione
     class com
     {
         private readonly System.Net.IPAddress _multicastAddress;
+        private readonly int port;
         private readonly User currentUser;
         public com()
         {
+            port = 44666;
             _multicastAddress = System.Net.IPAddress.Parse("239.165.41.74");
             currentUser = new User();
             currentUser.name = "Tonino";
@@ -94,7 +98,7 @@ namespace ProveTecniche_di_Trasmissione
         
         public async void LAN_Advertise()
         {
-            var endPoint = new System.Net.IPEndPoint(_multicastAddress, 10666);
+            var endPoint = new System.Net.IPEndPoint(_multicastAddress, port);
             var client = new System.Net.Sockets.UdpClient(System.Net.Sockets.AddressFamily.InterNetwork);
             client.JoinMulticastGroup(_multicastAddress);
             IFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
@@ -111,7 +115,7 @@ namespace ProveTecniche_di_Trasmissione
 
         public bool LAN_Listen()
         {
-            System.Net.Sockets.UdpClient client = new UdpClient(10666);
+            System.Net.Sockets.UdpClient client = new UdpClient(port);
             var endPoint = new System.Net.IPEndPoint(IPAddress.Any, 0);
             client.JoinMulticastGroup(_multicastAddress);
 
