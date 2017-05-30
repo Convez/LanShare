1e99bd9c4f5801579062ffe1b7221b2397d72e96
Convezione <enrico.panetta@gmail.com>
2017-05-30 16:31:06 +0200

    Prova di Annuncio/Ascolto host base sulla rete + prova serializzazione

--- LANshare/ProveTecniche di Trasmissione/Program.cs
+++ LANshare/ProveTecniche di Trasmissione/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using LANshare.Model;
 
 namespace ProveTecniche_di_Trasmissione
 {
@@ -25,24 +28,29 @@ namespace ProveTecniche_di_Trasmissione
             t.Wait();
             
         }
-
-        
     }
 
     class com
     {
         private readonly System.Net.IPAddress _multicastAddress;
-
+        private readonly User currentUser;
         public com()
         {
             _multicastAddress = System.Net.IPAddress.Parse("239.165.41.74");
+            currentUser = new User();
+            currentUser.name = "Tonino";
+            currentUser.surname = "Accolla";
         }
         public async void LAN_Advertise()
         {
             var endPoint = new System.Net.IPEndPoint(_multicastAddress, 10666);
             var client = new System.Net.Sockets.UdpClient(System.Net.Sockets.AddressFamily.InterNetwork);
             client.JoinMulticastGroup(_multicastAddress);
-            var data = Encoding.Default.GetBytes("Giovanni Muciaccia");
+            IFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            formatter.Serialize(ms, currentUser);
+            var data = ms.ToArray();
+            ms.Dispose();
             while (true)
             {
                 await client.SendAsync(data, data.Length, endPoint);
@@ -55,11 +63,16 @@ namespace ProveTecniche_di_Trasmissione
             System.Net.Sockets.UdpClient client = new UdpClient(10666);
             var endPoint = new System.Net.IPEndPoint(IPAddress.Any, 0);
             client.JoinMulticastGroup(_multicastAddress);
+
+            IFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
             while (true)
             {
                 var data = client.Receive(ref endPoint);
-                var message = Encoding.Default.GetString(data);
-                Console.WriteLine(message);
+                using (MemoryStream ms = new MemoryStream(data))
+                {
+                    User u = (User) formatter.Deserialize(ms);
+                    Console.WriteLine(endPoint.Address.ToString() + @" " + u.ToString());
+                }
             }
             return true;
         }
---
+++ LANshare/ProveTecniche di Trasmissione/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LANshare.Model
+{
+    [Serializable]
+    public class User
+    {
+        public string name { get; set; }
+        
+
+        public string surname { get; set; }
+        
+        
+
+        public override string ToString()
+        {
+            return name + " " + surname;
+        }
+    }
+}
